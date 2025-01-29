445a9944404a58dd44d020a71d5219d91ab30715
daniel <estudiolucho@gmail.com>
2025-01-28 23:40:27 -0500

    otro modif

--- app/Cash_Flow.php
+++ app/Cash_Flow.php
@@ -8,4 +8,5 @@ class Cash_Flow extends Model
 {
     protected $table = "cash_flow";
     protected $fillable = ['id','date','amount','concept','type','description' ,'balance','user_create','user_update','created_at'];
+//comment
 }
